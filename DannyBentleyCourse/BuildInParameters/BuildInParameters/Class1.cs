c71607eee1ef7ee8c923f25d3ce92381e767669d
Martin Stacey <martin__stacey@hotmail.com>
2019-07-21 14:41:09 -0500

    Add files via upload

---
+++ DannyBentleyCourse/BuildInParameters/BuildInParameters/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System.Linq;
+using System.Text;
+
+
+namespace GetSetParameters
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            Element e = SelectElement(uidoc, doc);
+            Parameter parameter = e.get_Parameter(BuiltInParameter.WALL_BASE_OFFSET);
+
+            using (Transaction t = new Transaction(doc, "param"))
+            {
+                t.Start("param");
+                try
+                {
+                    parameter.Set(-5);
+                }
+                catch { }
+                t.Commit();
+                TaskDialog.Show("new value", GetParameterValue(parameter));
+            }
+
+            return Result.Succeeded;
+        }
+        public Element SelectElement(UIDocument uidoc, Document doc)
+        {
+            Reference reference = uidoc.Selection.PickObject(ObjectType.Element);
+            Element element = uidoc.Document.GetElement(reference);
+            return element;
+        }
+        public string GetParameterValue(Parameter parameter)
+        {
+            switch (parameter.StorageType)
+            {
+                case StorageType.Double:
+                    return parameter.AsValueString();
+                case StorageType.ElementId:
+                    return parameter.AsElementId().IntegerValue.ToString();
+                case StorageType.Integer:
+                    return parameter.AsValueString();
+                case StorageType.None:
+                    return parameter.AsValueString();
+                case StorageType.String:
+                    return parameter.AsString();
+                default:
+                    return "";
+
+
+
+            }
+        }
+    }
+}
---
+++ DannyBentleyCourse/GetSetParameters/GetSetParameters/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System.Linq;
+using System.Text;
+
+
+namespace GetSetParameters
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            Element e = SelectElement(uidoc, doc);
+            Parameter parameter = e.LookupParameter("Comment");
+
+            using (Transaction t = new Transaction(doc,"parameter"))
+            {
+                t.Start("param");
+                try
+                {
+                    parameter.Set("test comment");
+                }
+                catch { }
+                t.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+        public Element SelectElement (UIDocument uidoc, Document doc)
+        {
+            Reference reference = uidoc.Selection.PickObject(ObjectType.Element);
+            Element element = uidoc.Document.GetElement(reference);
+            return element;
+        }
+    }
+}
