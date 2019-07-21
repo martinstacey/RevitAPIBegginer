60864177ddf6bb369ff8d3a0ba07f858f94c0f1d
Martin Stacey <martin__stacey@hotmail.com>
2019-07-21 14:47:34 -0500

    Add files via upload

---
+++ DannyBentleyCourse/TabyButton2/TabyButton2/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.ApplicationServices;
+
+
+namespace TabyButton2
+{
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Revit", "Hello World");
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+    }
+}
